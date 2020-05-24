e196b3489e4bd746697c4f64828ffeb41e8c759a
Omar Contreras <Contreras.arroyo.omar.3g@gmail.com>
2020-05-24 14:29:35 -0600

    Trabajo

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-//nos traemos al express
-
 const express = require('express');
-
-//creamos el express
 
 const app = express();
 
-//decimos que vamos a usar un sitio estatico de express en la carpeta public
-
 app.use(express.static(__dirname + '/public'));
-
-//hacemos que escuche el puerto 3000 y que cuando lo escuche lo imprima en la consola
 
 app.listen(3000, ()=>{
 
